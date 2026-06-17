c2344bf199398309238e3c0b50763526656804e4
Anushka12344 <4000anushkashah@gmail.com>
2026-06-17 21:04:26 +0545

    Add files via upload

---
+++ 1q.php
@@ -0,0 +1,20 @@
+<?DOCTYPE html>
+<html>
+<head>
+     <title> LAB </title>
+</head>
+<body>
+<?php
+echo "Hello World "."<br>";
+//print "Hello World"
+#Comment
+
+$x= print "Anuska <br>";
+echo $x;
+/*
+This code print "Anuska" and stores the return value (1) in $x.
+Then it display the value of $x.
+*/
+?>
+</body>
+</html>
